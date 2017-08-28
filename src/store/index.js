51ffb794e3a3d019b7ac91fb8f2a771951886b58
Javran Cheng <Javran.c@gmail.com>
2017-08-27 23:41:20 -0400

    store shape

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,22 @@
 import { createStore } from 'redux'
+import { mkSimpleReducer } from 'subtender'
 
-const reducer = (state = null, _action) => state
+const initState = {
+  params: {
+    dropRateRaw: '5%',
+    experimentCount: 1000,
+    beforeAbort: 2000,
+  },
+  result: {
+    // TODO
+    stats: null,
+  },
+}
+
+const reducer = mkSimpleReducer(
+  initState,
+  'DropCalc@modify'
+)
 
 const store = createStore(reducer)
 
