5bacf9a6b2352c46f229ff21cef66e4045bee6e5
Javran Cheng <Javran.c@gmail.com>
2017-08-27 23:52:48 -0400

    layout editor for exp count & before abort

--- src/ui/index.js
+++ src/ui/index.js
@@ -5,7 +5,7 @@ import { ParamsEditor } from './params-editor'
 class DropCalcMain extends Component {
   render() {
     return (
-      <div style={{padding: 10, minWidth: 600}}>
+      <div style={{padding: 10, maxWidth: 800}}>
         <ParamsEditor />
         <div>Result placeholder</div>
       </div>
--- src/ui/params-editor.js
+++ src/ui/params-editor.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react'
 import {
-  Panel,
+  Panel, FormControl, ButtonGroup, Button,
 } from 'react-bootstrap'
 
 const mkParamRow = (paramName, editor, key) => (
-  <div style={{display: 'flex', alignItems: 'center'}} key={key}>
+  <div style={{display: 'flex', alignItems: 'center', marginBottom: 5}} key={key}>
     <div style={{width: '16em'}}>
       {paramName}
     </div>
@@ -27,11 +27,33 @@ class ParamsEditor extends Component {
               ],
               [
                 'Experiment Count',
-                'Edit',
+                (
+                  <div style={{display: 'flex', alignItems: 'center'}}>
+                    <FormControl
+                      style={{flex: 1, marginRight: 5}}
+                      type="number"
+                      placeholder="TODO"
+                    />
+                    <Button style={{marginRight: 5}}>
+                      1000
+                    </Button>
+                    <Button style={{marginRight: 5}}>
+                      10000
+                    </Button>
+                    <Button>
+                      100000
+                    </Button>
+                  </div>
+                ),
               ],
               [
                 'Count Before Aborting',
-                'Edit',
+                (
+                  <FormControl
+                    type="number"
+                    placeholder="TODO"
+                  />
+                ),
               ],
             ].map((args, ind) =>
               mkParamRow(...args, ind)
