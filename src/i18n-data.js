56648fb3a9bac533b3e61e8fcca418c0b572f914
Javran Cheng <Javran.c@gmail.com>
2017-08-30 14:44:54 -0400

    i18n stuff

---
+++ src/i18n-data.js
@@ -0,0 +1,43 @@
+const trData = {
+  'en-US': {
+    Params: {
+      Title: "Parameters",
+      DropRate: "Drop Rate",
+      ExperimentCount: "Experiment Count",
+      BeforeAbort: "Attempts Before Aborting",
+      Simulate: "Simulate",
+    },
+    Result: {
+      Title: "Result",
+      CD: "CD",
+    },
+  },
+  'zh-CN': {
+    Params: {
+      Title: "参数",
+      DropRate: "掉落率",
+      ExperimentCount: "实验次数",
+      BeforeAbort: "尝试次数上限",
+      Simulate: "模拟",
+    },
+    Result: {
+      Title: "结果",
+      CD: "累积分布",
+    },
+  },
+  'zh-TW': {
+    Params: {
+      Title: "參數",
+      DropRate: "掉落率",
+      ExperimentCount: "實驗次數",
+      BeforeAbort: "嘗試次數上限",
+      Simulate: "模擬",
+    },
+    Result: {
+      Title: "結果",
+      CD: "累積分佈",
+    },
+  },
+}
+
+export { trData }
